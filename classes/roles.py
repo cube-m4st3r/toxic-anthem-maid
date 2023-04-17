732b462971a83d9563aec924f266c5483069bcb4
cube-m4st3r <benjaminfinck02@googlemail.com>
2023-04-17 22:08:55 +0200

    added buttons class to load

--- classes/roles.py
+++ classes/roles.py
@@ -1,8 +1,9 @@
 class Roles:
-    def __init__(self, role_id=None, role_name=None):
+    def __init__(self, role_id=None, role_name=None, role_button=None):
         if role_id is not None:
             self.role_id = role_id
             self.role_name = role_name
+            self.role_button = role_button
         else:
             pass
 
@@ -12,8 +13,14 @@ class Roles:
     def get_role_name(self):
         return self.role_name
 
+    def get_role_button(self):
+        return self.role_button
+
     def set_role_id(self, role_id):
         self.role_id = role_id
 
     def set_role_name(self, role_name):
         self.role_name = role_name
+
+    def set_role_button(self, role_button):
+        self.role_button = role_button
